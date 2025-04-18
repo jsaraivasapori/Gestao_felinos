62dc31c56a400d4dec5eaec0e236377c22e07b1c
João Vitor <joaosaraivasapori@gmail.com>
2025-04-18 16:38:34 -0300

    edit-felinos

---
+++ gestao_front/src/app/pages/felinos/edit-felinos/edit-felinos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-felinos',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-felinos.component.html',
+  styleUrl: './edit-felinos.component.scss'
+})
+export class EditFelinosComponent {
+
+}
